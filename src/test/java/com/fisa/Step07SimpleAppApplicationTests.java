2cdfb5af1284e79dea407b8330a4f4ead5a574e7
khk <aibigdatakr@gmail.com>
2023-08-28 14:51:16 +0900

    test1

---
+++ src/test/java/com/fisa/Step07SimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step07SimpleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
